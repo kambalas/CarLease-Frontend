5d33abe1d800112cbf132698688e8bbe10f2bc0d
Darius <mlecka.darius@gmail.com>
2024-04-10 16:38:36 +0300

    header and footer are displayed in the app

--- tarzanas-lizing/src/app/app.component.ts
+++ tarzanas-lizing/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { MainContainerComponent } from './main-container/main-container.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainContainerComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
