b2c8f5d42627ab17c05c2dc95e4ee71432299727
GabrieleJasiunaite <jasinait.gabriel@gmail.com>
2024-04-10 21:22:25 +0300

    add partial form functionality

--- tarzanas-lizing/src/app/components/car-leasing-calculator/car-leasing-calculator.component.ts
+++ tarzanas-lizing/src/app/components/car-leasing-calculator/car-leasing-calculator.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-car-leasing-calculator',
   standalone: true,
-  imports: [MatSlideToggleModule],
+  imports: [MatSlideToggleModule, ReactiveFormsModule],
   templateUrl: './car-leasing-calculator.component.html',
   styleUrl: './car-leasing-calculator.component.scss'
 })
 export class CarLeasingCalculatorComponent {
+  calculatorForm = this.makeForm();
+
+  private makeForm() {
+    return new FormGroup(
+      {
+        carValue: new FormControl(''),
+        period: new FormControl('months'),
+        downpayment: new FormControl(''),
+        residual: new FormControl('0'),
+        envFriendly: new FormControl(false),
+      });
+  }
+
+  onSubmit() {
+    console.log(this.calculatorForm.value);
+  }
 
 }
