8d652e75aba7988bfa49805fcd0d3f2ad0abaf20
GabrieleJasiunaite <jasinait.gabriel@gmail.com>
2024-04-10 20:48:53 +0300

    add car leasing calculator html

---
+++ tarzanas-lizing/src/app/components/car-leasing-calculator/car-leasing-calculator.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-car-leasing-calculator',
+  standalone: true,
+  imports: [MatSlideToggleModule],
+  templateUrl: './car-leasing-calculator.component.html',
+  styleUrl: './car-leasing-calculator.component.scss'
+})
+export class CarLeasingCalculatorComponent {
+
+}
--- tarzanas-lizing/src/app/main-container/main-container.component.ts
+++ tarzanas-lizing/src/app/main-container/main-container.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HowItWorksComponent } from '../how-it-works/how-it-works.component';
+import { HowItWorksComponent, CarLeasingCalculatorComponent } from '../how-it-works/how-it-works.component';
+import { CarLeasingCalculatorComponent } from "../components/car-leasing-calculator/car-leasing-calculator.component";
 
 
 
 @Component({
   selector: 'app-main-container',
   standalone: true,
-  imports: [RouterOutlet,HowItWorksComponent],
   templateUrl: './main-container.component.html',
-  styleUrl: './main-container.component.scss'
+  styleUrl: './main-container.component.scss',
+  imports: [RouterOutlet, HowItWorksComponent, CarLeasingCalculatorComponent]
 })
 export class MainContainerComponent {
 
