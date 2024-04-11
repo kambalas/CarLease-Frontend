6d19c1bf3ee203313189563eb74a096656e30efb
Darius <mlecka.darius@gmail.com>
2024-04-11 11:09:01 +0300

    how it works works

--- tarzanas-lizing/src/app/main-container/main-container.component.ts
+++ tarzanas-lizing/src/app/main-container/main-container.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HowItWorksComponent } from '../how-it-works/how-it-works.component';
 
 
 
 @Component({
   selector: 'app-main-container',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HowItWorksComponent],
   templateUrl: './main-container.component.html',
   styleUrl: './main-container.component.scss'
 })
