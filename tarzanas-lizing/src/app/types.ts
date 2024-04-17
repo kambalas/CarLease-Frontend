59c3472f96845b67daaa7882af827baff4bde236
andriusLedas <152397675+andriusLedas@users.noreply.github.com>
2024-04-17 13:13:22 +0300

    Add:  Application interface

--- tarzanas-lizing/src/app/types.ts
+++ tarzanas-lizing/src/app/types.ts
@@ -3,8 +3,8 @@ export interface CalculatorFormFields {
   period: string;
   downPayment: number;
   residualValuePercentage: string;
-  isEcoFriendly: boolean,
-  monthlyPayment: string
+  isEcoFriendly: boolean;
+  monthlyPayment: string;
 }
 
 export interface CalculatorRequest {
@@ -12,44 +12,44 @@ export interface CalculatorRequest {
   period: string;
   downPayment: number;
   residualValuePercentage: string;
-  isEcoFriendly: boolean
+  isEcoFriendly: boolean;
 }
 
 export interface CalculatorResponse {
-  monthlyPayment: number
+  monthlyPayment: number;
 }
 
 export interface PersonalInformationFormFields {
-  firstName: string,
-  lastName: string,
-  email: string,
-  phoneNumber: string,
-  pid: string,
-  dateOfBirth: number,
-  maritalStatus: string,
-  numberOfChildren: number,
-  citizenship: string,
-  monthlyIncome: number
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  pid: string;
+  dateOfBirth: number;
+  maritalStatus: string;
+  numberOfChildren: number;
+  citizenship: string;
+  monthlyIncome: number;
 }
 
 export interface CarLeasingFormFields {
-  make: string,
-  model: string
-  modelVariant: string,
-  year: string,
-  fuelType: string,
-  enginePower: number,
-  engineSize: number,
-  url: string,
-  offer: string,
-  terms: boolean,
-  confirmation: boolean
+  make: string;
+  model: string;
+  modelVariant: string;
+  year: string;
+  fuelType: string;
+  enginePower: number;
+  engineSize: number;
+  url: string;
+  offer: string;
+  terms: boolean;
+  confirmation: boolean;
 }
 
 export interface FormsPostRequest {
-  ratesRequest: CalculatorFormFields,
-  personalInformationRequest: PersonalInformationFormFields,
-  leaseRequest: CarLeasingFormFields
+  ratesRequest: CalculatorFormFields;
+  personalInformationRequest: PersonalInformationFormFields;
+  leaseRequest: CarLeasingFormFields;
 }
 
 export interface Car {
@@ -92,4 +92,19 @@ export interface personalInfoDetails {
   childrenCount: number;
   citizenship: string[];
   montlyIncome: number;
+}
+export interface Application {
+  id: string;
+  firstName: string;
+  secondName: string;
+  dateSubmitted: Date;
+  dateUpdated: Date;
+  status: Status;
+}
+export const enum Status {
+  NEW = 'new',
+  UPDATED = 'updated',
+  PENDING = 'pending',
+  ACCEPTED = 'accepted',
+  REJECTED = 'rejected',
 }
