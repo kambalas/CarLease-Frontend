5aafe01f40c0102ae067b20a64dedf037eee1ba2
GabrieleJasiunaite <jasinait.gabriel@gmail.com>
2024-04-17 13:41:23 +0300

    add basic html for displaying data

--- tarzanas-lizing/src/app/components/admin/single-application-view/single-application-view.component.ts
+++ tarzanas-lizing/src/app/components/admin/single-application-view/single-application-view.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-single-application-view',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatDividerModule],
   templateUrl: './single-application-view.component.html',
   styleUrl: './single-application-view.component.scss'
 })
