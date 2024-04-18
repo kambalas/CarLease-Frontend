df79d12b0e5a92683ce0014fab04689dad4719d2
Darius <mlecka.darius@gmail.com>
2024-04-18 21:29:21 +0300

    send button at notes

--- tarzanas-lizing/src/app/components/admin/notes-tab/notes-tab.component.ts
+++ tarzanas-lizing/src/app/components/admin/notes-tab/notes-tab.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-notes-tab',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule,MatCardModule],
   templateUrl: './notes-tab.component.html',
   styleUrl: './notes-tab.component.scss'
 })
