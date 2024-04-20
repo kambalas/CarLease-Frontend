63464f8583902449371a7b11ba7df600ebcc03bb
37060 <patricija.katinaite@gmail.com>
2024-04-20 21:41:40 +0300

    Add dropdown for mail box which lets you choose mail template

--- tarzanas-lizing/src/app/components/admin/mail-tab/mail-tab.component.ts
+++ tarzanas-lizing/src/app/components/admin/mail-tab/mail-tab.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatOption} from "@angular/material/autocomplete";
+import {MatSelect} from "@angular/material/select";
 
 @Component({
   selector: 'app-mail-tab',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, FormsModule, MatFormField, MatLabel, MatOption, MatSelect, ReactiveFormsModule],
   templateUrl: './mail-tab.component.html',
   styleUrl: './mail-tab.component.scss',
 })
-export class MailTabComponent { }
+export class MailTabComponent {
+
+    selectedTemplate: string = '';
+}
