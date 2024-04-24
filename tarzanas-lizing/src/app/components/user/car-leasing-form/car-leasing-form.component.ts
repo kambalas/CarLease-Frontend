a7b0f436f087ce7fea68c3d6036a02e4c7ead960
Motiejūnas Ugnius <motiejunasugnius@gmail.com>
2024-04-24 10:50:22 +0300

    base64 ecnoded pdf

--- tarzanas-lizing/src/app/components/user/car-leasing-form/car-leasing-form.component.ts
+++ tarzanas-lizing/src/app/components/user/car-leasing-form/car-leasing-form.component.ts
@@ -90,7 +90,7 @@ export class CarLeasingFormComponent implements OnInit {
           enginePower: '',
           engineSize: '',
           url: '',
-          offer: undefined,
+          offer: '',
           terms: false,
           confirmation: false,
         });
@@ -161,11 +161,29 @@ export class CarLeasingFormComponent implements OnInit {
   }
 
   onFileSelected(event: any): void {
-    this.selectedFile = event.target.files[0] ?? null;
+    const file = event.target.files[0] ?? null;
+    if (file) {
+        this.convertFileToBase64(file, (base64: string) => {
+            this.base64File = base64;
+            console.log('File in Base64:', this.base64File);
+        });
+    } else {
+        this.base64File = '';
+    }
 }
 
   getButtonColor() {
     return this.carLeasingForm.valid ? 'primary' : 'warn';
   }
-
+  convertFileToBase64(file: File, callback: (result: string) => void): void {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+        const base64String = reader.result as string;
+        callback(base64String.split(',')[1]);  // Split and remove the data URL prefix
+    };
+    reader.onerror = (error) => {
+        console.error('Error converting file:', error);
+    };
+}
 }
