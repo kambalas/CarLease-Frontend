c5697c92f077b98704dcf803808715604bae1768
Edvinas <edvinas.gerdvila@mif.stud.vu.lt>
2024-04-26 16:51:14 +0300

    Added multi lingual for first stage

--- tarzanas-lizing/src/app/app.config.ts
+++ tarzanas-lizing/src/app/app.config.ts
@@ -1,11 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './interceptors/auth.interceptor';
+import {routes} from './app.routes';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {HttpClientModule, provideHttpClient, withInterceptors} from '@angular/common/http';
+import {authInterceptor} from './interceptors/auth.interceptor';
+import {AppTranslateModule} from "./modules/app-translate.module";
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(withInterceptors([authInterceptor]))]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(AppTranslateModule.forRoot())
+  ]
 };
--- tarzanas-lizing/src/app/components/user/how-it-works/how-it-works.component.ts
+++ tarzanas-lizing/src/app/components/user/how-it-works/how-it-works.component.ts
@@ -1,9 +1,12 @@
 import { Component, ViewChild } from '@angular/core';
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-how-it-works',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule
+  ],
   templateUrl: './how-it-works.component.html',
   styleUrl: './how-it-works.component.scss'
 })
